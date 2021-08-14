b40ab412291f85aeb324b75b5c5a2da2c8377ee0
ukshat <33008157+ukshat@users.noreply.github.com>
2021-08-14 11:26:57 -0700

    Added Example Op Mode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lab01_ExampleOpMode.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class Lab01_ExampleOpMode extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+        }
+
+    }
+}
